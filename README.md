6a34bcbefcc2705976d4e9d8118c01d4cf0b59c5
Takashi Shinohara <takashi.shinohara@outlook.com>
2025-01-29 18:10:38 +0900

    0.3.0 release

--- README.md
+++ README.md
@@ -5,71 +5,103 @@
 
 [Zenn](https://zenn.dev/) のアイキャッチ絵文字を記事の内容から生成するためのプラグインです。
 
-## 使用方法
+## 事前準備
 
 *このプラグインでは OpenAI の GPT を使用しますが環境は提供していません。OpenAI または Azure OpenAI Service の環境をあらかじめご用意ください。また、このプラグインを実行したことによるいかなる損害も責任を負いません。*
 
-1. Zenn の GitHub 連携を行っているリポジトリにパッケージをインストールします。
+Zenn の GitHub 連携を行っているリポジトリにパッケージをインストールします。
+
+```
+npm install @karamem0/zenn-plugin-emoji
+```
+
+`.env` または `.env.local` ファイルを作成し以下の環境変数を設定します。
+
+|キー名|説明|OpenAI|Azure OpenAI Service (API キー)|Azure OpenAI Service (Entra ID アプリケーション)|
+|-|-|-|-|-|
+|OPENAI_API_KEY|OpenAI の API キー|X|||
+|OPENAI_API_VERSION|OpenAI の API バージョン (`2024-05-01` など)||X|X|
+|OPENAI_MODEL_NAME|OpenAI の場合はモデル名、Azure OpenAI Serviceの場合はデプロイ名|X|X|X|
+|AZURE_OPENAI_API_KEY|Azure OpenAI Service の API キー||X||
+|AZURE_OPENAI_ENDPOINT|Azure OpenAI Service のエンドポイント||X|X|
+|AZURE_CLIENT_ID|Entra ID アプリケーションのクライアント ID|||X|
+|AZURE_CLIENT_SECRET|Entra ID アプリケーションのクライアント シークレット|||X|
+|AZURE_TENANT_ID|Entra ID アプリケーションのテナント ID|||X|
+
+以下に OpenAI および Azure OpenAI Service 環境ごとの設定例を示します。
+
+- **OpenAI の例**
 
     ```
-    npm install @karamem0/zenn-plugin-emoji
+    OPENAI_API_KEY=<your-openai-api-key>
+    OPENAI_MODEL_NAME=<your-openai-model-name>
     ```
 
-1. `.env` または `.env.local` ファイルを作成し以下の環境変数を設定します。
-
-    |キー名|説明|OpenAI|Azure OpenAI Service (API キー)|Azure OpenAI Service (Entra ID アプリケーション)|
-    |-|-|-|-|-|
-    |OPENAI_API_KEY|OpenAI の API キー|X|||
-    |OPENAI_API_VERSION|OpenAI の API バージョン (`2024-05-01` など)||X|X|
-    |OPENAI_MODEL_NAME|OpenAI の場合はモデル名、Azure OpenAI Serviceの場合はデプロイ名|X|X|X|
-    |AZURE_OPENAI_API_KEY|Azure OpenAI Service の API キー||X||
-    |AZURE_OPENAI_ENDPOINT|Azure OpenAI Service のエンドポイント||X|X|
-    |AZURE_CLIENT_ID|Entra ID アプリケーションのクライアント ID|||X|
-    |AZURE_CLIENT_SECRET|Entra ID アプリケーションのクライアント シークレット|||X|
-    |AZURE_TENANT_ID|Entra ID アプリケーションのテナント ID|||X|
-
-    以下に OpenAI および Azure OpenAI Service 環境ごとの設定例を示します。
-
-    - **OpenAI の例**
-
-        ```
-        OPENAI_API_KEY=<your-openai-api-key>
-        OPENAI_MODEL_NAME=<your-openai-model-name>
-        ```
-
-    - **Azure OpenAI Service (API キー) の例**
-
-        ```
-        AZURE_OPENAI_API_KEY=<your-aoai-api-key>
-        AZURE_OPENAI_ENDPOINT=https://<your-aoai-resoure-name>.openai.azure.com
-        OPENAI_API_VERSION=<your-aoai-api-version>
-        OPENAI_MODEL_NAME=<your-aoai-deployment-name>
-        ```
-
-    - **Azure OpenAI Service (Entra ID アプリケーション) の例**
-
-        ```
-        AZURE_CLIENT_ID=<your-app-client-id>
-        AZURE_CLIENT_SECRET=<your-app-client-secret>
-        AZURE_TENANT_ID=<your-app-tenant-id>
-        AZURE_OPENAI_ENDPOINT=https://<your-aoai-resoure-name>.openai.azure.com
-        OPENAI_API_VERSION=<your-aoai-api-version>
-        OPENAI_MODEL_NAME=<your-aoai-deployment-name>
-        ```
-
-        Entra ID アプリケーションに対して **Cognitive Services OpenAI User** のロールの割り当てが必要です。
-
-1. コマンドを実行します。
+- **Azure OpenAI Service (API キー) の例**
 
     ```
-    npx zenn-emoji articles/* -u
+    AZURE_OPENAI_API_KEY=<your-aoai-api-key>
+    AZURE_OPENAI_ENDPOINT=https://<your-aoai-resoure-name>.openai.azure.com
+    OPENAI_API_VERSION=<your-aoai-api-version>
+    OPENAI_MODEL_NAME=<your-aoai-deployment-name>
     ```
 
-    既定ではアイキャッチ絵文字が空欄の場合のみ更新します。アイキャッチ絵文字が設定されているファイルも更新したい場合は `-f` オプションを追加してください。
+- **Azure OpenAI Service (Entra ID アプリケーション) の例**
+
+    Entra ID アプリケーションに対して **Cognitive Services OpenAI User** のロールの割り当てが必要です。
+
+    ```
+    AZURE_CLIENT_ID=<your-app-client-id>
+    AZURE_CLIENT_SECRET=<your-app-client-secret>
+    AZURE_TENANT_ID=<your-app-tenant-id>
+    AZURE_OPENAI_ENDPOINT=https://<your-aoai-resoure-name>.openai.azure.com
+    OPENAI_API_VERSION=<your-aoai-api-version>
+    OPENAI_MODEL_NAME=<your-aoai-deployment-name>
+    ```
+
+## 使用方法
+
+### アイキャッチ絵文字を生成する
+
+既定ではアイキャッチ絵文字が存在しないファイルを対象にします。オプションを付けない場合はコンソールに結果のみを表示しファイルは更新しません。
+
+```bash
+npx zenn-emoji articles/*
+articles/ebd6b7f4e118f3.md: 💻 プログラミングを学ぶというテーマからコンピュータをイメージしました。
+```
+
+### 生成したアイキャッチ絵文字でファイルを更新する
+
+`-u` オプションを付けることで生成したアイキャッチ絵文字でファイルを更新します。
+
+```bash
+npx zenn-emoji articles/* -u
+```
+
+### 前回の結果でファイルを更新する
+
+`-l` オプションを付けることで前回の実行結果でファイルを更新します。すぐにファイルを更新せずに結果を確認してから更新する場合におすすめです。
+
+```bash
+npx zenn-emoji articles/*       # 生成結果を確認して
+npx zenn-emoji articles/* -u -l # ファイルを更新する
+```
+
+### すべてのファイルを対象にする
+
+`-f` オプションを付けることでアイキャッチ絵文字が存在するファイルも対象にします。
+
+```bash
+npx zenn-emoji articles/* -f
+articles/1d7a517be7f801.md: ⏳ 非同期処理という言葉から時間の経過をイメージしました。
+articles/261cbfd985d9b8.md: 🌐 AIというテーマから、世界中に広がる技術の可能性をイメージしました。
+articles/ebd6b7f4e118f3.md: 💻 プログラミングというテーマから、コンピュータをイメージしました。
+```
 
 ## オプション
 
 |短い名前|長い名前|説明|
 |-|-|-|
 |`-u`|`--update`|対象のファイルを更新します。|
+|`-l`|`--use-last-exec`|前回の実行結果を使用します。|
 |`-f`|`--force`|アイキャッチ絵文字が設定されているファイルの更新を強制します。|
--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,9 @@
 
 import { AzureOpenAI, OpenAI } from 'openai';
 import { ClientSecretCredential, getBearerTokenProvider } from '@azure/identity';
+import { readFile, writeFile } from 'fs/promises';
 import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
-import fs from 'fs';
 import path from 'path';
 import { program } from 'commander';
 
@@ -21,10 +21,24 @@ dotenv.config({ path: ['.env', '.env.local'] });
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-interface Option {
-  force: boolean,
-  update: boolean
+interface ChatResponse {
+  emoji: string,
+  reason: string
 }
+
+interface CommandOptions {
+  force?: boolean,
+  update?: boolean,
+  useLastExec?: boolean
+}
+
+interface LastExecLogItem {
+  file: string,
+  emoji: string,
+  reason: string
+}
+
+type LastExecLog = LastExecLogItem[];
 
 function createOpenAI(): OpenAI {
   if (process.env.OPENAI_API_KEY) {
@@ -66,48 +80,96 @@ function createOpenAI(): OpenAI {
   throw new Error('Cannot create an instance of OpenAI');
 }
 
+async function callOpenAI(text: string): Promise<ChatResponse> {
+  if (!openai) throw new Error('OpenAI is not initialized');
+  if (!text) throw new Error('Text is required');
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: 'system',
+        content: prompt
+      },
+      {
+        role: 'user',
+        content: text
+      }
+    ],
+    model: process.env.OPENAI_MODEL_NAME,
+    response_format: {
+      type: 'json_object'
+    }
+  });
+  return JSON.parse(completion.choices[0].message.content) as ChatResponse;
+}
+
+async function readPrompt(): Promise<string> {
+  return await readFile(path.join(__dirname, 'skills/skprompt.txt'), 'utf-8');
+}
+
+async function readLastExecLog(): Promise<LastExecLog> {
+  try {
+    return JSON.parse(await readFile(path.join(__dirname, 'lastexeclog.json'), 'utf-8'));
+  } catch {
+    return [];
+  }
+}
+
+async function writeLastExecLog(value: LastExecLog): Promise<void> {
+  await writeFile(path.join(__dirname, 'lastexeclog.json'), JSON.stringify(value), 'utf-8');
+}
+
+const openai = createOpenAI();
+const prompt = await readPrompt();
+const lastExecLog = await readLastExecLog();
+
 program
   .version(process.env.npm_package_version)
   .argument('<files...>', 'The target files')
   .option('-f, --force', 'Force to update the target files')
+  .option('-l, --use-last-exec', 'Use last executed values')
   .option('-u, --update', 'Update the target files')
-  .action(async (files: string[], option: Option) => {
-    const prompt = await fs.promises.readFile(path.join(__dirname, 'skills/skprompt.txt'), 'utf-8');
-    const openai = createOpenAI();
-    files.forEach(async (file) => {
-      try {
-        const text = await fs.promises.readFile(file, 'utf-8');
-        if (!option.force && /emoji: ".+"/u.test(text)) {
-          return;
+  .action(async (files: string[], options: CommandOptions) => {
+    const { force, update, useLastExec } = options;
+    await Promise.all(await Promise.all(files
+      .map(async (file) => {
+        try {
+          return {
+            file,
+            text: await readFile(file, 'utf-8')
+          };
+        } catch (error) {
+          console.error(`${file}: failed to read file\n${error instanceof Error ? error.message : error}`);
         }
-        const completion = await openai.chat.completions.create(
-          {
-            messages: [
-              {
-                role: 'system',
-                content: prompt
-              },
-              {
-                role: 'user',
-                content: text
-              }
-            ],
-            model: process.env.OPENAI_MODEL_NAME,
-            response_format: {
-              type: 'json_object'
-            }
-          }
-        );
-        const content = JSON.parse(completion.choices[0].message.content);
-        console.log(`${file}: ${content.emoji} ${content.reason}`);
-        if (option.update) {
-          await fs.promises.writeFile(file, text.replace(/emoji: ".*"/u, `emoji: "${content.emoji}"`), 'utf-8');
+      }))
+      .then(items => items.filter(({ text }) => force || /emoji: ""/u.test(text)))
+      .then(async items => await Promise.all(items.map(async ({ file, text }) => {
+        try {
+          return {
+            file,
+            text,
+            ...useLastExec
+              ? lastExecLog.find(item => item.file === file)
+              : await callOpenAI(text)
+          };
+        } catch (error) {
+          console.error(`${file}: failed to call OpenAI API\n${error instanceof Error ? error.message : error}`);
         }
-      } catch (error) {
-        if (error instanceof Error) {
-          console.error(`${file}: ${error.message}`);
+      })))
+      .then(items => items.filter(({ emoji }) => emoji))
+      .then(items => items.map(async ({ file, text, emoji, reason }) => {
+        try {
+          console.log(`${file}: ${emoji} ${reason}`);
+          if (update) await writeFile(file, text.replace(/emoji: ".*"/u, `emoji: "${emoji}"`), 'utf-8');
+          return {
+            file,
+            emoji,
+            reason
+          };
+        } catch (error) {
+          console.error(`${file}: failed to write file\n${error instanceof Error ? error.message : error}`);
         }
-      }
-    });
+      })))
+      .then(async items => !useLastExec && await writeLastExecLog(items))
+      .catch(error => console.error(error instanceof Error ? error.message : error));
   })
   .parse(process.argv);
